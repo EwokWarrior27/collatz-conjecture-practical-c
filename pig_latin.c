76fd2abb223a2df703a0befb62b48294713529c6
Arjun <silverarjun27@gmail.com>
2020-11-04 08:29:48 -0800

    Update pig_latin.c

--- pig_latin.c
+++ pig_latin.c
@@ -5,10 +5,10 @@
 
 char* convert_to_lowercase(char* str) {
 	char *str2 = strdup(str);
-for (int i = 0; str2[i]; i++) {
-	str2[i] = tolower(str2[i]);
-}
-return str2;
+	for (int i = 0; str2[i]; i++) {
+		str2[i] = tolower(str2[i]);
+	}
+	return str2;
 }
 
 char* word_to_pig_latin(char* str) {
@@ -18,20 +18,20 @@ char* word_to_pig_latin(char* str) {
 		strcat(new_str, “way”);
 		new_str[strlen(str) + 3] = 0;
 		return new_str;
-}
-else {
-	int cap = isupper(str[0]);
-	int nConsonant = 0;
-	for(int i = 0; i < strlen(str); i++){
-		if(tolower(str[i]) != ‘a’ && tolower(str[i]) != ‘e’ && tolower(str[i]) != ‘i’ && tolower(str[0]) != ‘o’ && tolower(str[0]) != ‘u’){
-			break;
-		}
-		nConsonant++;
 	}
-char* new_str = malloc(strlen(str) + 3);
-strncpy(new_str, &str[nConsonant], strlen(str) - nConsonant);
-strncat(new_str, str, nConsonant);
-strcat(new_str, “ay\0”);
-return new_str;
-}
+	else {
+		int cap = isupper(str[0]);
+		int nConsonant = 0;
+		for(int i = 0; i < strlen(str); i++){
+			if(tolower(str[i]) != ‘a’ && tolower(str[i]) != ‘e’ && tolower(str[i]) != ‘i’ && tolower(str[0]) != ‘o’ && tolower(str[0]) != ‘u’){
+				break;	
+			}
+			nConsonant++;
+		}
+		char* new_str = malloc(strlen(str) + 3);
+		strncpy(new_str, &str[nConsonant], strlen(str) - nConsonant);
+		strncat(new_str, str, nConsonant);
+		strcat(new_str, “ay\0”);
+		return new_str;
+	}
 }
